7d1266a4c1e04a77f19b668380336c4c90c5e24d
LyDeming <ly.c.d.deming@gmail.com>
2021-12-19 16:12:46 -0500

    styling and logic redo

--- script.js
+++ script.js
@@ -1,69 +1,64 @@
 // rock paper scissors game //
-for (i=0; i<=5; i++) {
+const playerDisplay = document.getElementById('playerSelection');
+const computerDisplay = document.getElementById("computerSelction");
+const result = document.getElementById("result")
+const choices = document.querySelectorAll("button");
+let playerAnswer
+let randomChoice
+
+choices.forEach(choice => choice.addEventListener('click', (e) => {
+    playerAnswer = e.target.id
+    playerDisplay.innerHTML = playerAnswer
+    computerSelection()
+}))
     
-    function getPlayerInput() {
-        let playerInput = prompt ("Rock, paper, or scissors? Choose wisely").toLowerCase();
-        return playerInput;
-    }    
-
-    let playerAnswer = getPlayerInput()
-
-    console.log (playerAnswer)
-
-    function computerSelection() {   
-        const cSelection = ["rock", "paper", "scissors"]; 
-        let randomItem = cSelection[Math.floor(Math.random()*cSelection.length)];{
-        return randomItem;
-        }
-    }   
-
-    let computerAnswer = computerSelection();
-
-    console.log (computerAnswer);
-
-    function playRound(playerAnswer, computerAnswer) {
-        if (playerAnswer === computerAnswer) {
-            return "Great minds think alike";
-        }  else if (playerAnswer === "rock" && computerAnswer === "scissors") {
-            return "You win! Rock smashes scissors";
-        }  else if (playerAnswer === "rock" && computerAnswer === "paper") {
-            return "You lose! Paper smothers rock";
-        }  else if (playerAnswer === "paper" && computerAnswer === "scissors") {
-            return "You lose! Scissors cuts paper";
-        }  else if (playerAnswer === "paper" && computerAnswer === "rock") {
-            return "You win! Paper smothers rock";
-        }  else if (playerAnswer === "scissors" && computerAnswer === "paper") {
-            return "You Win! Scissors cuts paper";
-        }  else (playerAnswer === "scissors" && computerAnswer === "rock") 
-            return "You lose! Rock smashes scissors"; 
-    }
-
-    let round = playRound(playerAnswer, computerAnswer)
-    console.log (round);
-
-    function getScore(addScore) {
-        switch (round){
-            case "You win! Rock smashes scissors":
-                //fall through//
-            case "You win! Paper smothers rock":
-                //fall through//
-            case "You Win! Scissors cuts paper":
-                return 1;
-                break;
-            case "You lose! Rock smashes scissors":
-                //fall through//
-            case "You lose! Paper smothers rock":
-                //fall through//
-            case "You lose! Scissors cuts paper":
-                //fall through//
-            case "Great minds think alike": 
-                return 0;  
-                break;    
-        }
-    }
-
-    console.log (getScore());
+function computerSelection() {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    computerDisplay.innerHTML = randomChoice
 }
 
+function playRound(playerAnswer, computerAnswer) {
+    if (playerAnswer === computerAnswer) {
+        return "Great minds think alike! It's a draw.";
+    } else if (playerAnswer === "rock" && computerAnswer === "scissors") {
+        return "You win! Rock smashes scissors.";
+    } else if (playerAnswer === "rock" && computerAnswer === "paper") {
+        return "You lose! Paper smothers rock.";
+    } else if (playerAnswer === "paper" && computerAnswer === "scissors") {
+        return "You lose! Scissors cuts paper.";
+    } else if (playerAnswer === "paper" && computerAnswer === "rock") {
+        return "You win! Paper smothers rock.";
+    } else if (playerAnswer === "scissors" && computerAnswer === "paper") {
+        return "You Win! Scissors cuts paper.";
+    } else (playerAnswer === "scissors" && computerAnswer === "rock")
+    return "You lose! Rock smashes scissors.";
+}
 
-//console.log (getScore())
+/*let round = playRound(playerAnswer, computerAnswer)
+console.log(round);
+
+function getScore(addScore) {
+    switch (round) {
+        case "You win! Rock smashes scissors":
+        //fall through//
+        case "You win! Paper smothers rock":
+        //fall through//
+        case "You Win! Scissors cuts paper":
+            return 1;
+            break;
+        case "You lose! Rock smashes scissors":
+        //fall through//
+        case "You lose! Paper smothers rock":
+        //fall through//
+        case "You lose! Scissors cuts paper":
+        //fall through//
+        case "Great minds think alike":
+            return 0;
+            break;
+    }
+}
+
+console.log(getScore());
+
+
+//console.log (getScore())*/
